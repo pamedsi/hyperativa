8877166c68ca0b2c1792b050669c6358f2c6b949
Patrick Melo <patrickabimael@yahoo.com.br>
2025-09-26 22:11:32 -0300

    ♻️ refactor: Removes * from imports

--- src/main/java/com/hyperativa/card/application/api/CardAPI.java
+++ src/main/java/com/hyperativa/card/application/api/CardAPI.java
@@ -3,7 +3,11 @@ package com.hyperativa.card.application.api;
 
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 
 @RequestMapping("/card")
