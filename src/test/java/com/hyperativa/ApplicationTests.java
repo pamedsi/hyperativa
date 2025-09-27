417a489224649e18cf2c50c83df4d75e004473c0
Patrick Melo <patrickabimael@yahoo.com.br>
2025-09-27 02:53:16 -0300

    🔧 chore: Configures unit tests

--- src/test/java/com/hyperativa/ApplicationTests.java
+++ src/test/java/com/hyperativa/ApplicationTests.java
@@ -1,13 +1,23 @@
 package com.hyperativa;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ApplicationContext context;
+
+    @Test
+    void contextLoads() {
+        assertThat(context).isNotNull();
+    }
 
 }
